ed9b636f10e00b366827226d6968743ca05fed33
anbinh93 <an.nb225591@sis.hust.edu.vn>
2024-11-23 11:45:59 +0700

    update feature search cart

--- AIMSProject/src/Cart.java
+++ AIMSProject/src/Cart.java
@@ -76,30 +76,30 @@ public class Cart {
        System.out.println("***************************************************");
    }
 
-//    public void searchById(int id) {
-//        boolean found = false;
-//        for (int i = 0; i < qtyOrdered; i++) {
-//            if (itemsOrdered[i].getId() == id) {
-//                System.out.println(itemsOrdered[i].toString());
-//                found = true;
-//            }
-//        }
-//        if (!found) {
-//            System.out.println("No match found for ID: " + id);
-//        }
-//    }
+   public void searchById(int id) {
+       boolean found = false;
+       for (int i = 0; i < qtyOrdered; i++) {
+           if (itemsOrdered[i].getId() == id) {
+               System.out.println(itemsOrdered[i].toString());
+               found = true;
+           }
+       }
+       if (!found) {
+           System.out.println("No match found for ID: " + id);
+       }
+   }
 
-//    public void searchByTitle(String title) {
-//        boolean found = false;
-//        for (int i = 0; i < qtyOrdered; i++) {
-//            if (itemsOrdered[i].getTitle().equalsIgnoreCase(title)) {
-//                System.out.println(itemsOrdered[i].toString());
-//                found = true;
-//            }
-//        }
-//        if (!found) {
-//            System.out.println("No match found for title: " + title);
-//        }
-//    }
+   public void searchByTitle(String title) {
+       boolean found = false;
+       for (int i = 0; i < qtyOrdered; i++) {
+           if (itemsOrdered[i].getTitle().equalsIgnoreCase(title)) {
+               System.out.println(itemsOrdered[i].toString());
+               found = true;
+           }
+       }
+       if (!found) {
+           System.out.println("No match found for title: " + title);
+       }
+   }
 }
 
