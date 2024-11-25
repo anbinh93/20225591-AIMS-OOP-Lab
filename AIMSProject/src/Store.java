ddd3828e1715ec05eb938d5e645008ad1d265aed
anbinh93 <an.nb225591@sis.hust.edu.vn>
2024-11-25 11:16:27 +0700

    update store topic

---
+++ AIMSProject/src/Store.java
@@ -0,0 +1,29 @@
+public class Store {
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[100];
+    private int qtyInStore = 0;
+
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore < itemsInStore.length) {
+            itemsInStore[qtyInStore] = dvd;
+            qtyInStore++;
+            System.out.println("The DVD has been added to the store.");
+        } else {
+            System.out.println("The store is full.");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i].equals(dvd)) {
+                for (int j = i; j < qtyInStore - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[qtyInStore - 1] = null;
+                qtyInStore--;
+                System.out.println("The DVD has been removed from the store.");
+                return;
+            }
+        }
+        System.out.println("The DVD is not in the store.");
+    }
+}
